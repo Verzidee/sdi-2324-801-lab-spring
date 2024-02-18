fe7c2d2ef79f6664c9ee3b27fdbe52860fb6e239
Verzidee <Zohaib0614@gmail.com>
2024-02-18 18:59:26 +0100

    SDI-2324-801-4.4-SpringBoot-Consultas.

--- src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
+++ src/main/java/com/uniovi/notaneitor/controllers/MarksController.java
@@ -1,6 +1,7 @@
 package com.uniovi.notaneitor.controllers;
 
 import com.uniovi.notaneitor.entities.Mark;
+import com.uniovi.notaneitor.entities.User;
 import com.uniovi.notaneitor.services.MarksService;
 import com.uniovi.notaneitor.services.UsersService;
 import com.uniovi.notaneitor.validators.MarkAddValidator;
@@ -12,6 +13,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -29,8 +31,10 @@ public class MarksController {
         this.httpSession = httpSession;
     }
     @RequestMapping("/mark/list")
-    public String getList(Model model) {
-        model.addAttribute("markList",marksService.getMarks());
+    public String getList(Model model, Principal principal) {
+        String dni = principal.getName(); // DNI es el name de la autenticación
+        User user = usersService.getUserByDni(dni);
+        model.addAttribute("markList",marksService.getMarksForUser(user));
         return "mark/list";
     }
     @RequestMapping("/mark/details/{id}")
@@ -77,8 +81,10 @@ public class MarksController {
         return "redirect:/mark/details/"+id;
     }
     @RequestMapping("/mark/list/update")
-    public String updateList(Model model){
-        model.addAttribute("markList", marksService.getMarks() );
+    public String updateList(Model model, Principal principal){
+        String dni = principal.getName(); // DNI es el name de la autenticación
+        User user = usersService.getUserByDni(dni);
+        model.addAttribute("markList", marksService.getMarksForUser(user) );
         return "mark/list :: marksTable";
     }
     //Poner a true el atributo resend
